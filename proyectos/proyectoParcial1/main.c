15ef8f891ffacfb7623f3d0a67328c6354bcc831
Oscar Hinojosa <oscarjl13@gmail.com>
2018-02-15 23:21:50 +0000

    proyecto

---
+++ proyectos/proyectoParcial1/main.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#define SIZEINPUTARRAY 	10001
+#define SIZEOUTPUTARRAY 80
+#define STARTMAYUS 		65
+#define FINISHMAYUS 	90
+#define STARTMINUS 		97
+#define FINISHMINUS 	122
+#define STARTNUMS		48
+#define FINISHNUMS		57
+#define SCHAR1			33  /*!*/
+#define SCHAR2			63  /*?*/
+#define SCHAR3			58  /*:*/
+#define SCHAR4			46  /*.*/
+#define SCHAR5			45  /*-*/
+#define SCHAR6			59  /*;*/
+#define SCHAR7			44  /*,*/
+#define SCHAR8			32  /* */
+#define SCHARNEXT		62  /*>*/
+#define SCHARBACK		60	/*<*/
+
+int checkMayus (unsigned char * ptrArray);
+int checkMinus(unsigned char * ptrArray);
+int checkNums(unsigned char * ptrArray);
+int checkSpecialchars(unsigned char * ptrArray);
+int proccesData(unsigned char * ptrinArray,unsigned char * ptroutArray);
+static char inputArray[SIZEINPUTARRAY];
+static char outputArray[SIZEOUTPUTARRAY];
+
+
+static void getText(unsigned char * ptrArray)
+{
+	printf("%s\n","inserta texto:" );
+	
+	fgets(ptrArray, (SIZEINPUTARRAY) , stdin);
+	printf("%i\n",10001);
+}
+
+static int  checkArray4ValidData (unsigned char * ptrArray)
+{	
+	unsigned char data2return = 0;
+	do{
+		data2return |=    checkMayus( ptrArray ) 
+						| checkMinus( ptrArray ) 
+						| checkNums( ptrArray )
+						| checkSpecialchars( ptrArray );
+	
+	}while((*ptrArray++) != EOF );
+	printf("%i", data2return);
+	return data2return;
+
+}
+
+int main ()
+{
+	int value= 0;
+	getText(inputArray);
+	printf("%s\n", inputArray );
+
+	if(0 != checkArray4ValidData(inputArray) )
+	{
+		printf("%s\n", "Error input");
+		return 0; // se que esto no se hace pero no tuve tiempo de corregirlo
+	}
+	value = proccesData(inputArray,outputArray);
+	if(value)
+	{
+		printf("%s\n","Error procesando datos" );
+		return 0; // se que esto no se hace pero no tuve tiempo de corregirlo
+	}
+	printf("%s\n",outputArray );
+
+	return 0;
+}
+
+int proccesData(unsigned char * ptrinArray,unsigned char * ptroutArray)
+{
+	unsigned char data2return = 2;
+	unsigned char spacefree = SIZEOUTPUTARRAY;
+	unsigned char * ptrArrayout2move=ptroutArray;
+	printf("%s\n","proess data" );
+	do{
+			printf("%d", spacefree );
+		if(0 == spacefree)
+		{
+			ptrArrayout2move=ptroutArray;
+			spacefree = SIZEOUTPUTARRAY;
+		}
+
+
+		if ((SCHARBACK == *ptrinArray))
+		{	
+			if (0 < spacefree )
+			{
+				spacefree++;
+				ptrArrayout2move--;
+
+			}
+			
+		}
+		else if (SCHARNEXT == *ptrinArray)
+		{
+			spacefree--;
+			ptrArrayout2move++;
+		}
+		else
+		{
+			spacefree--;
+			(*ptroutArray) = (*ptrinArray) ;
+		
+			printf("%s",(*ptrinArray));
+			ptrArrayout2move++;
+		}
+		
+	}while((*ptrinArray++) != EOF );
+
+	return data2return;
+
+}
+
+
+
+
+int checkMayus (unsigned char * ptrArray)
+{
+	unsigned char data2return = 2;
+
+	if (STARTMAYUS <*ptrArray < FINISHMAYUS )
+	{
+		data2return = 0;
+	}
+	else
+	{
+		data2return = 1;
+	}
+	return data2return;
+}
+
+int checkMinus(unsigned char * ptrArray)
+{
+	unsigned char data2return = 2;
+	if (STARTMINUS <*ptrArray < FINISHMINUS )
+	{
+		data2return = 1;
+	}
+	else
+	{
+		data2return = 0;
+	}
+	return data2return;
+}
+
+int checkNums(unsigned char * ptrArray)
+{
+	unsigned char data2return = 2;
+	if (STARTNUMS <*ptrArray < FINISHNUMS )
+	{
+		data2return = 0;
+	}
+	else
+	{
+		data2return = 1;
+	}
+	return data2return;
+}
+
+int checkSpecialchars(unsigned char * ptrArray)
+{
+	unsigned char data2return = 2;
+
+	if (	( SCHAR1 == *ptrArray ) 
+		|| 	( SCHAR2 == *ptrArray ) 
+		|| 	( SCHAR3 == *ptrArray ) 
+		|| 	( SCHAR4 == *ptrArray ) 
+		||  ( SCHAR5 == *ptrArray )
+		||  ( SCHAR6 == *ptrArray )
+		||  ( SCHAR7 == *ptrArray )
+		||  ( SCHARNEXT == *ptrArray )
+		||  ( SCHARBACK == *ptrArray )
+		)
+	{
+		data2return = 0;
+	}
+	else
+	{
+		data2return = 1;
+	}
+	return data2return;
+}
+
---
+++ proyectos/proyectoParcial1/maintest.tcl
@@ -0,0 +1,22 @@
+#!C:\cygwin64\bin\expect.exe
+# For colors
+proc capability cap {expr {![catch {exec tput -S << $cap}]}}
+proc colorterm {} {expr {[capability setaf] && [capability setab]}}
+proc tput args {exec tput -S << $args >/dev/tty}
+array set color {black 0 red 1 green 2 yellow 3 blue 4 magenta 5 cyan 6 white 7}
+proc foreground x {exec tput -S << "setaf $::color($x)" > /dev/tty}
+proc background x {exec tput -S << "setab $::color($x)" > /dev/tty}
+proc reset {} {exec tput sgr0 > /dev/tty}
+
+
+#Put your test case here
+eval spawn [lrange $argv 0 end]
+
+expect "inserta texto:" {foreground green; puts "PASSED";reset} default {foreground red;puts "FAILED";reset}
+send "holas2222.\n"
+expect "holas2222." {foreground green; puts "PASSED";reset} default {foreground red;puts "FAILED";reset}
+
+
+
+#send ">><<<Look for clothes at the <<<<<<<<<<<<<<<second floor. <<<<<<<Fresh pizza and <<<<<<<<<<<<<<<<hamburger at a shop right to <<<<<<<<<<<<<the entrance. Call <<<<<<<<<< 123<-456<-8790 <<<<<<<<<<<<<<<<to order <<<<<<<<<<<<<<<<<computers< and office<<<<<<< chairs.\n"
+#expect "Look for second hamburger at computer and chairs.790" {foreground green; puts "PASSED";reset} default {foreground red;puts "FAILED";reset}
