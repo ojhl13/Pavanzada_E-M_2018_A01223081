0948a3fe8a9cf17494102fd063f3b66c7a86c54f
Oscar Hinojosa <oscarjl13@gmail.com>
2018-03-22 20:31:56 -0600

    mediapractica11

---
+++ read_student.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct s
+{
+	char name 		[20];
+	char lastName 	[20];
+	unsigned int id;
+	unsigned char cal;
+}Student;
+
+
+int main ( int argc, char **argv)
+{
+
+	Student myStudent;
+	char *lastName = "Perez2";
+	
+
+
+	return 0;
+}
--- subir_avances.sh
+++ subir_avances.sh
@@ -5,3 +5,5 @@ git add .
 git commit -m "$1"
 git push
 
+echo ojhl13
+echo retorfe1010
