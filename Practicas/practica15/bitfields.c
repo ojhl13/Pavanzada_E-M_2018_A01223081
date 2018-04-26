8f14d93fab1b1d2fa95710157797c4dbe8d914f3
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2018-04-26 18:37:26 -0500

    pratica 0x0f bitfield

---
+++ Practicas/practica15/bitfields.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+typedef strcut d{
+  unsigned char day:5;
+  unsigned char month:4;
+  unsigned short year;
+} Date;
+
+int main(int argc, char const *argv[]) {
+  Date today = {26,4,2018};
+  printf("%s %d %d %d\n", "Today is ",today.day,today.month,today.year  );
+  return 0;
+}
