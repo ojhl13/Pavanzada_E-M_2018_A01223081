58df580ba92ae799b01a07752e04a36d3f0301f9
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2018-04-26 18:38:40 -0500

    opeartion of bits

--- Practicas/practica15/bitfields.c
+++ Practicas/practica15/bitfields.c
@@ -8,5 +8,9 @@ typedef strcut d{
 int main(int argc, char const *argv[]) {
   Date today = {26,4,2018};
   printf("%s %d %d %d\n", "Today is ",today.day,today.month,today.year  );
+  if( 26 ^= today.day)
+  {
+    printf("%s\n", "Today is 26" );
+  }
   return 0;
 }
